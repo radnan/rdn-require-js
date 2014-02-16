b6e7db4faaf7c2b6656c780f41f83244286c427e
radnan <rafiadnan47@gmail.com>
2014-02-16 14:14:03 -0600

    requireJS view helper

--- src/RdnRequireJS/Factory/View/Helper/RequireJS.php
+++ src/RdnRequireJS/Factory/View/Helper/RequireJS.php
@@ -9,5 +9,16 @@ class RequireJS extends AbstractFactory
 {
 	protected function create()
 	{
+		$config = $this->config('rdn_require_js');
+
+		if (PHP_SAPI == 'cli')
+		{
+			$helpers = $this->service('ViewHelperManager');
+			$config['config']['baseUrl'] = call_user_func($helpers->get('BasePath'), rtrim($config['config']['baseUrl'], '/'));
+		}
+
+		$library = $config['config']['baseUrl'] .'/'. ltrim($config['library'], '/');
+
+		return new Helper\RequireJS(new ConfigScript($config), $library);
 	}
 }
--- src/RdnRequireJS/View/Helper/RequireJS.php
+++ src/RdnRequireJS/View/Helper/RequireJS.php
@@ -2,9 +2,72 @@
 
 namespace RdnRequireJS\View\Helper;
 
+use RdnRequireJS\ConfigScript;
 use Zend\View\Helper\AbstractHelper;
 use Zend\View\Renderer\PhpRenderer;
 
 class RequireJS extends AbstractHelper
 {
+	/**
+	 * @var ConfigScript
+	 */
+	protected $config;
+
+	/**
+	 * @var string
+	 */
+	protected $library;
+
+	/**
+	 * @var bool
+	 */
+	protected $libraryIncluded = false;
+
+	/**
+	 * @var PhpRenderer
+	 */
+	protected $view;
+
+	/**
+	 * @param ConfigScript $config
+	 * @param string $library Path to requireJS library
+	 */
+	public function __construct(ConfigScript $config, $library)
+	{
+		$this->config = $config;
+		$this->library = $library;
+	}
+
+	/**
+	 * @return ConfigScript
+	 */
+	public function getConfig()
+	{
+		return $this->config;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getLibrary()
+	{
+		return $this->library;
+	}
+
+	/**
+	 * @param string|string[] $dependencies
+	 */
+	public function __invoke($dependencies = array())
+	{
+		$this->config->addDependencies($dependencies);
+
+		if ($this->libraryIncluded)
+		{
+			$this->view->inlineScript()
+				->appendScript($this->config)
+				->appendFile($this->library);
+
+			$this->libraryIncluded = true;
+		}
+	}
 }
